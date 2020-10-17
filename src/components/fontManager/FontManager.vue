6932c5cdb6a8b44532e77669feb7b5d023a69883
atomgomba <karcsi@ekezet.com>
2020-10-17 11:50:06 +0200

    Port FontManager and LogoManager to Vue

---
+++ src/components/fontManager/FontManager.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="fontmanagercontent" style="display:none; width:720px;">
+        <div class="font-picker" style="margin-bottom: 10px;">
+            <h1 class="tab_title" i18n="osdSetupFontPresets"></h1>
+            <!-- Font preview and list -->
+            <label class="font-manager-version-info"></label>
+            <div class="content_wrapper font-preview"></div>
+            <div class="fontpresets_wrapper">
+                <label id="font-selector-label" i18n="osdSetupFontPresetsSelector"></label>
+                <select class="fontpresets">
+                    <option value="-1" disabled i18n="osdSetupFontPresetsSelectorCustomOption"></option>
+                    <!-- Other values populated at runtime -->
+                </select>
+                <span id="font-selector-span" i18n="osdSetupFontPresetsSelectorOr"> Or </span>
+            </div>
+
+            <!-- Boot logo setup -->
+            <LogoManager />
+            
+            <!-- Upload progress bar -->
+            <div class="info">
+                <a name="progressbar"></a>
+                <progress class="progress" value="0" min="0" max="100"></progress>
+                <div class="progressLabel" style="margin-top: -21px; width: 95%; text-align: center; position: absolute;"></div>
+            </div>
+        </div>
+        <!-- Upload button -->
+        <div class="default_btn green" style="width:100%; float:left;
+            ">
+            <a class="flash_font active" i18n="osdSetupUploadFont"></a>
+        </div>
+    </div>    
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import LogoManager from 'logoManager/LogoManager.vue'
+export default Vue.extend({
+    components: {
+        'LogoManager': LogoManager,
+    },
+})
+</script>
---
+++ src/components/fontManager/logoManager/LogoManager.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- Boot logo setup -->
+    <div>
+    <h1 class="tab_title" i18n="osdSetupCustomLogoTitle"></h1>
+    <div class="grid-row">
+        <div id="font-logo-preview-container" class="content_wrapper grid-col col6">
+            <div id="font-logo-preview">
+                <!-- this will be resized at runtime -->
+            </div>
+        </div>
+        <div id="font-logo-info grid-col col6">
+            <h3 i18n="osdSetupCustomLogoInfoTitle"></h3>
+            <ul>
+                <li id="font-logo-info-size" i18n="osdSetupCustomLogoInfoImageSize"></li>
+                <li id="font-logo-info-colors" i18n="osdSetupCustomLogoInfoColorMap"></li>
+            </ul>
+            <p id="font-logo-info-upload-hint" i18n="osdSetupCustomLogoInfoUploadHint"></p>
+        </div>
+    </div>
+    <!-- Replace logo button -->
+    <div class="default_btn" style="width:100%; float:left;">
+        <a class="replace_logo" i18n="osdSetupCustomLogoOpenImageButton"></a>
+    </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        font: null,
+    },
+})
+</script>
